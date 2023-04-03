126bc0aaec7d4dd7472bf85c8af7dab4c783edf2
megaseves <megaseves@gmail.com>
2023-04-03 11:27:43 +0200

    Add: album image and design

--- src/audios/audios.js
+++ src/audios/audios.js
@@ -1,10 +1,14 @@
 exports.songsdata = [
     {
         "title": "Fekete krónika",
+        "album_title": "Túlzó láng",
+        "album_url": `${process.env.PUBLIC_URL + '/albumBorito.jpg'}`,
         "url": "https://red-pumpkin-songs.s3.eu-north-1.amazonaws.com/Red_Pumpkin_-_Fekete_kronika.wav"
     },
     {
         "title": "Bukott angyal",
+        "album_title": "Túlzó láng",
+        "album_url": `${process.env.PUBLIC_URL + '/albumBorito.jpg'}`,
         "url": "https://red-pumpkin-songs.s3.eu-north-1.amazonaws.com/Red_Pumpkin_-_Bukott_angyal.wav"
     }
 ]
--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,6 +1,6 @@
 import './Player.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPause, faPlay} from "@fortawesome/free-solid-svg-icons";
+import {faBackwardStep, faForwardStep, faPause, faPlay, faRepeat, faShuffle} from "@fortawesome/free-solid-svg-icons";
 
 export function Player({audioElem, isPlaying, setIsPlaying, currentSong}) {
 
@@ -9,6 +9,7 @@ export function Player({audioElem, isPlaying, setIsPlaying, currentSong}) {
     }
 
   return (
+
       <div className="player-container" >
 
           <div className="player">
@@ -17,18 +18,42 @@ export function Player({audioElem, isPlaying, setIsPlaying, currentSong}) {
                 </div>
               <div className="audio-detail">
                   <h2 className={"audio-detail-title"}>{currentSong.title}</h2>
+                  <h4 className={"audio-detail-album-name"}>Red Pumpkin</h4>
+                  <div className="audio-detail-time">
+                    <span className={"time-current"}>0:07</span>
+                    <div className={"time-line"}></div>
+                    <span className={"time-over"}>4:10</span>
+                  </div>
                   <div className="audio-controls">
+                      <FontAwesomeIcon className={"play-audio small-control"} icon={faShuffle} />
+                      <FontAwesomeIcon className={"play-audio medium-control"} icon={faBackwardStep} />
                       {isPlaying ?
                           <FontAwesomeIcon className={"play-audio"} icon={faPause} onClick={() => PlayPause()} />
                           :
                           <FontAwesomeIcon className={"play-audio"} icon={faPlay} onClick={() => PlayPause()} />
                       }
+                      <FontAwesomeIcon className={"play-audio medium-control"} icon={faForwardStep} />
+                      <FontAwesomeIcon className={"play-audio small-control"} icon={faRepeat} />
                   </div>
+              </div>
+              <div className="up-next-container">
+                  <h3>Up Next</h3>
               </div>
 
           </div>
 
           <div className="album-name-and-releases">
+            <div className="album-container">
+                <img className={"album-image"} src={currentSong.album_url} alt="" />
+                <div className="album-description">
+                    <h3>{currentSong.album_title} album</h3>
+                    <p>Listen to all songs from this album</p>
+                </div>
+            </div>
+
+              <div className="new-releases">
+                  <h3>New Releases</h3>
+              </div>
 
           </div>
 
