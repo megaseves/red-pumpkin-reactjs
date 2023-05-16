aba3c9b7b2278665018a80d7b0d133718394a0b9
megaseves <megaseves@gmail.com>
2023-05-16 11:46:27 +0200

    Fix: bottom player design

--- src/pages/Player/PlayerBottomComponent.js
+++ src/pages/Player/PlayerBottomComponent.js
@@ -22,35 +22,44 @@ export function PlayerBottomComponent({isPlaying, PlayPause, skipBack, skipForwa
                 <div className="time-line-bottom" style={{width: `${currentSong.progress+"%"}`}}></div>
                 <div className="time-line-bottom-bg"></div>
             </div>
-            <div className="play-and-pause">
-                <FontAwesomeIcon className={"play-audio medium-control"} icon={faBackwardStep} onClick={() => skipBack()} />
-                {isPlaying ?
-                  <FontAwesomeIcon className={"play-audio"} icon={faPause} onClick={() => PlayPause()} />
-                  :
-                  <FontAwesomeIcon className={"play-audio"} icon={faPlay} onClick={() => PlayPause()} />
-                }
-                <FontAwesomeIcon className={"play-audio medium-control"} icon={faForwardStep} onClick={() => skipForward()} />
-            </div>
-            <div className="time-container-bottom-player">
-                <span className={"time-current"}>{ currentSong.seconds ? converter(Math.floor(currentSong.seconds)) : "0:00"}</span>
-                <span>/</span>
-                <span className={"time-over"}>{ minutes ? minutes + ":" + seconds : "0:00"}</span>
-            </div>
-            <div className="title-container">
-                <img className="release-song-image" src={currentSong.album_url} alt=""></img>
-                <div className="song-title-and-album">
-                    <div className="song-title"><h3>{audio.title}</h3></div>
-                    <div className="album-title"><p>{audio.album_title}</p></div>
+
+            <div className="player-bottom-content-position">
+                <div className="player-bottom-left">
+                    <div className="play-and-pause">
+                        <FontAwesomeIcon className={"play-audio medium-control"} icon={faBackwardStep} onClick={() => skipBack()} />
+                        {isPlaying ?
+                            <FontAwesomeIcon className={"play-audio"} icon={faPause} onClick={() => PlayPause()} />
+                            :
+                            <FontAwesomeIcon className={"play-audio"} icon={faPlay} onClick={() => PlayPause()} />
+                        }
+                        <FontAwesomeIcon className={"play-audio medium-control"} icon={faForwardStep} onClick={() => skipForward()} />
+                    </div>
+                    <div className="time-container-bottom-player">
+                        <span className={"time-current"}>{ currentSong.seconds ? converter(Math.floor(currentSong.seconds)) : "0:00"}</span>
+                        <span>/</span>
+                        <span className={"time-over"}>{ minutes ? minutes + ":" + seconds : "0:00"}</span>
+                    </div>
+
                 </div>
-                <div className="settings">
-                    <FontAwesomeIcon className={"play-audio small-control"} icon={faEllipsisVertical} />
+
+                <div className="title-container">
+                    <img className="release-song-image" src={currentSong.album_url} alt=""></img>
+                    <div className="song-title-and-album">
+                        <div className="song-title"><h3>{audio.title}</h3></div>
+                        <div className="album-title"><p>{audio.album_title}</p></div>
+                    </div>
+                    <div className="settings">
+                        <FontAwesomeIcon className={"play-audio small-control"} icon={faEllipsisVertical} />
+                    </div>
+                </div>
+
+                <div className="other-controls">
+                    <FontAwesomeIcon id={'volume'} className={"play-audio small-control"} icon={faVolumeLow} />
+                    <FontAwesomeIcon className={"play-audio small-control"} icon={faRepeat} onClick={() => toggleRepeatBtn()} />
+                    <FontAwesomeIcon className={"play-audio small-control"} icon={faShuffle} onClick={() => shufflePlayList()} />
                 </div>
             </div>
-            <div className="other-controls">
-                <FontAwesomeIcon id={'volume'} className={"play-audio small-control"} icon={faVolumeLow} />
-                <FontAwesomeIcon className={"play-audio small-control"} icon={faRepeat} onClick={() => toggleRepeatBtn()} />
-                <FontAwesomeIcon className={"play-audio small-control"} icon={faShuffle} onClick={() => shufflePlayList()} />
-            </div>
+
         </div>
     );
 }
