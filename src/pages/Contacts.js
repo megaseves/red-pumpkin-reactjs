42344fe0cb71c30450df4120a8a77b6d6f4a3f68
megaseves <megaseves@gmail.com>
2023-10-05 20:53:59 +0200

    Add: contact page filled and change a tag design

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -9,7 +9,12 @@ export function Contacts() {
     
   return (
       <div className="contacts-container">
-
+          <div className='contacts'>
+            <h2>Megtalálsz Facebook-on vagy írj e-mailben!</h2>
+            <br />
+            <h2>Facebook: <a href='https://www.facebook.com/attila.schmiedt' target='_blank'>Schmiedt Attila</a></h2>
+            <h2>E-mail: <a href = "mailto: megaseves@gmail.com">megaseves@gmail.com</a></h2>
+          </div>
       </div>
   );
 }
