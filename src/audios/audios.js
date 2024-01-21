7a266855a7bce6b65f0159b0ae0ae854f4badcd8
Schmiedt Attila <95692218+megaseves@users.noreply.github.com>
2024-01-21 23:21:03 +0100

    Update audios.js
    Add belso haboru song

--- src/audios/audios.js
+++ src/audios/audios.js
@@ -1,5 +1,12 @@
 exports.songsdata = [
     {
+        "title": "Belső háború",
+        "album_title": "Túlzó láng",
+        "album_url": `${process.env.PUBLIC_URL + '/albumBorito.jpg'}`,
+        "url": "https://red-pumpkin-songs.s3.eu-north-1.amazonaws.com/Red_Pumpkin_-_Belso_haboru.wav",
+        "length": "3:32"
+    },
+    {
         "title": "Fekete krónika",
         "album_title": "Túlzó láng",
         "album_url": `${process.env.PUBLIC_URL + '/albumBorito.jpg'}`,
