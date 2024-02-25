19c7f0c46549bd859b7d4d7c28c59e0ffcb80caf
megaseves <megaseves@gmail.com>
2024-02-25 13:09:14 +0100

    Add: new email address

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -13,7 +13,7 @@ export function Contacts() {
             <h5>Megtalálsz Facebook-on vagy írj e-mail-t!</h5>
             <br />
             <h5>Facebook: <a href='https://www.facebook.com/attila.schmiedt' target='_blank'>Schmiedt Attila</a></h5>
-            <h5>E-mail: <a href = "mailto: megaseves@gmail.com">megaseves@gmail.com</a></h5>
+            <h5>E-mail: <a href = "mailto: schmiedt.attila95@gmail.com">schmiedt.attila95@gmail.com</a></h5>
           </div>
       </div>
   );
